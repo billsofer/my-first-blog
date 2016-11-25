6a7406c4246513dbdf239968f3a777b636650d3e
billsofer <billsofer@mac.com>
2016-11-25 16:01:47 -0600

    more changes

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -9,10 +9,24 @@ def post_list(request):
 	return render(request, 'blog/post_list.html', {'posts':posts})
 	
 def post_new(request):
-	form = PostForm()
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('detail', pk=post.pk)
+	else:
+		form = PostForm()
 	return render(request, 'blog/post_edit.html', {'form':form})
 	
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post':post})
-	
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
