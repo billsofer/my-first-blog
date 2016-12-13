f79974d5be5f5be0f9c0323e7ffd71a28f3aec03
billsofer <billsofer@mac.com>
2016-12-13 15:32:14 -0600

    more classes added

--- classes/models.py
+++ classes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+	name = models.CharField(max_length=128)
+	venue = models.CharField(max_length=64)
+	subject = models.CharField(max_length=64)
+	date = models.DateField('date presented')
+	
+	def __str__(self):
+		title = 'Name: %s' % (self.name)
+		title += ' - Venue: %s ' % (self.venue)
+		return title
+		
+class Slides(models.Model):
+	session = models.CharField(max_length=128)
+	keynote = models.URLField(max_length=256)
+	course = models.ForeignKey(Course, on_delete=models.CASCADE)
+	
+	def __str__(self):
+		return self.session
